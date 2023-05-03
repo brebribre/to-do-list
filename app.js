df261ae42d4af24a61b383827364bcf5dd333af3
brebribre <70563428+brebribre@users.noreply.github.com>
2023-05-03 21:41:40 +0200

    adjusted port for render

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3030;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -150,6 +151,6 @@ app.post("/delete", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
